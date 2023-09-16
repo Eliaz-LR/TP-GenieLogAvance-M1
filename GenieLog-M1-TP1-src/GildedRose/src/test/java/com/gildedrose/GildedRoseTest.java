0603864abd1952001d23198953767fa511e3729a
axelfvvv <129721072+axelfvvv@users.noreply.github.com>
2023-09-16 10:44:50 +0200

    1er test
    test about fixing quality  part 1
    (extremum)
    
    not a good test just to commit

--- GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,5 +13,33 @@ class GildedRoseTest {
     app.updateQuality();
     assertEquals("foo", element.name, "the name changed");
   }
+  /* 0% INCREASING JACOCO */
+  @DisplayName("Test that the quality of all item except Sulfura" +
+   "is between 0-50, sulfura is always at 80")
+   void testValueQualityExtr() {
+    Item element1 = new Item("foo", 3, 0);
+    Item element2 = new Item("Aged Brie", 4, 50);
+    Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50);
+
+    Item element4 = new Item("Sulfuras, Hand of Ragnaros", 20, 80);
+
+    GildedRose app1 = new GildedRose(new Item[] {element1});
+    GildedRose app2 = new GildedRose(new Item[] {element2});
+    GildedRose app3 = new GildedRose(new Item[] {element3});
+    GildedRose app4 = new GildedRose(new Item[] {element4});
+
+    app1.updateQuality();
+    app2.updateQuality();
+    app3.updateQuality();
+    app4.updateQuality();
+
+    assertEquals(0, element1.quality, "The quality is always positive");
+    assertEquals(50, element2.quality, "The quality can't be higher than 50");
+    assertEquals(50, element3.quality, "The quality can't be higher than 50");
+    assertEquals(80, element4.quality, "Sulfuras quality is always 80");
+   }
+   
+
+ 
 
 }
