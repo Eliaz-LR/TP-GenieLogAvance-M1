4fdac540a15126179d1b895606bffec422d8e850
axelfvvv <129721072+axelfvvv@users.noreply.github.com>
2023-09-19 17:36:28 +0200

    v3.0.1
    v3.0.1
    J'AI REUSSI !!! JE SUIS PASSE A 51%
    EN FAIT IL FAUT RAJOUTER @Test à chacun des tests
    
    // Fonctionne

--- GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,4 +13,27 @@ class GildedRoseTest {
     app.updateQuality();
     assertEquals("foo", element.name, "the name changed");
   }
+  @Test
+  @DisplayName("On gère d'abord les comportements des items normaux")
+  /*
+   * Propriétés à vérifier pour les cartes normales:
+   * Si SellIn < 0 alors un objet se dégrade 2x plus vite.
+   * La qualité est comprise entre 0 et 50 
+   */
+  void testBasicItem(){
+    Item element = new Item("foo", 1, 8);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(0, element.sellIn, "sellIn decreasing test");
+    assertEquals(7, element.quality, "quality decreasing test");
+    app.updateQuality();
+    assertEquals(-1, element.sellIn, "negative sellIn normal test");
+    assertEquals(5, element.quality, "negative sellIn / decreasing quality double");
+    element.quality = 0;
+    app.updateQuality();
+    assertEquals(0, element.quality, "quality negativity test");
+    
+
+  }
+
 }
