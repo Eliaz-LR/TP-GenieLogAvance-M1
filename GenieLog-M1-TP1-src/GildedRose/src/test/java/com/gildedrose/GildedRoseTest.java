b632a7f13b0ad644f8f154c9c6c0385d40061096
axelfvvv <129721072+axelfvvv@users.noreply.github.com>
2023-09-19 17:47:49 +0200

    v3.0.5
    ON AVANCE
    91% de réussite

--- GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -35,5 +35,34 @@ class GildedRoseTest {
     
 
   }
+  @Test
+  @DisplayName("Test the quality system of Backstage Passes")
 
+  void testPassesQuality() {
+     Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 17);
+     if(element.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+       for(int expDate = 0; expDate <= 11; expDate++){
+        element.quality = 17;
+        element.sellIn = expDate;
+        GildedRose app = new GildedRose(new Item[] {element});
+ 
+         if(expDate == 0){
+           app.updateQuality();
+           assertEquals(0, element.quality, "The concert is over");
+         }
+         else if(expDate <= 5){
+          app.updateQuality();
+          assertEquals(20, element.quality, "The concert is very near");
+         }
+         else if(expDate <=10){
+          app.updateQuality();
+          assertEquals(19, element.quality, "The concert is near");
+         }
+         else{
+          app.updateQuality();
+          assertEquals(18, element.quality,"the concert is far ahead");
+         }
+       }
+     }
+    }
 }
