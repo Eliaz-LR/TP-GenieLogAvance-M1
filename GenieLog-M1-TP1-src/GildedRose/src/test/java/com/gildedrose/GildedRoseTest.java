0397b99ca2f9842c01f3b996e69d3df5543c02f6
axelfvvv <129721072+axelfvvv@users.noreply.github.com>
2023-09-19 16:58:51 +0200

    Update 2 GildedRose
    Two different test that doesn't increase jococo green marks.

--- GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -39,7 +39,54 @@ class GildedRoseTest {
     assertEquals(80, element4.quality, "Sulfuras quality is always 80");
    }
    
+@DisplayName("Test the decreasing quality function")
 
- 
+void testDecreasingFunction() {
+  Item element1 = new Item("Foo", 4, 2);
+  Item element2 = new Item("Aged Brie", 4, 10);
+  Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 7);
+  Item element4 = new Item("Sulfuras, Hand of Ragnaros", 20, 80);
 
+  
+    GildedRose app1 = new GildedRose(new Item[] {element1});
+    GildedRose app2 = new GildedRose(new Item[] {element2});
+    GildedRose app3 = new GildedRose(new Item[] {element3});
+    GildedRose app4 = new GildedRose(new Item[] {element4});
+
+    app1.updateQuality();
+    app2.updateQuality();
+    app3.updateQuality();
+    app4.updateQuality();
+  
+    assertEquals(0, element1.quality, "Quality decreasing ok, foo");
+    assertEquals(11, element2.quality, "Quality decreasing ok, brie");
+    assertEquals(8, element3.quality, "Quality decreasing ok, backstage");
+    assertEquals(80, element4.quality, "Quality decreasing ok, Sulfuras");
 }
+
+
+ @DisplayName("Test the quality system of Backstage Passes")
+
+ void testPassesQuality() {
+    if(element.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+      for(int expDate = 0; expDate <= 11; expDate++){
+        Item element = new Item("Backstage passes to a TAFKAL80ETC concert", expDate, 17);
+    
+       GildedRose app = new GildedRose(new Item[] {element});
+        app.updateQuality();
+
+        if(expDate == 0){
+          assertEquals(0, element.quality, "The concert is over");
+        }
+        else if(expDate <= 5){
+         assertEquals(20, element.quality, "The concert is very near");
+        }
+        else if(expDate <=10){
+         assertEquals(19, element.quality, "The concert is near");
+        }
+        else{
+         assertEquals(18, element.quality,"the concert is far ahead");
+        }
+      }
+    }
+  } 
