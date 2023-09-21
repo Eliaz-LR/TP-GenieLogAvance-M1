07326e9e93d5f1702e9ee7edbc820dc05be798ac
axelfvvv <129721072+axelfvvv@users.noreply.github.com>
2023-09-21 06:41:15 +0200

    v 3.0.8 UNIT TEST FUNCTIONS
    + Que des fonctions unitaires
    - Pas optimisé (fonctions inutiles)
    - Fonction to_String

--- GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GenieLog-M1-TP1-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,125 +14,171 @@ class GildedRoseTest {
     assertEquals("foo", element.name, "the name changed");
   }
 
-
-  // 2 tests (minimum) mais 3 pour vérifier l'évolution du sellIn
-  @Test
-  @DisplayName("On gère d'abord les comportements des items normaux")
-  /*
-   * Propriétés à vérifier pour les cartes normales:
-   * Si SellIn < 0 alors un objet se dégrade 2x plus vite.
-   * La qualité est comprise entre 0 et 50 
+  
+  /* Test functions to test properties of basic items
+   * 3 tests. 1 optional test
    */
 
-  void testBasicItem(){
+  //optional test
+  @Test
+  @DisplayName("Test decreasing sellIn")
+  void diminutionsellIn(){
+    
     Item element = new Item("foo", 1, 8);
     GildedRose app = new GildedRose(new Item[] {element});
-
     app.updateQuality();
     assertEquals(0, element.sellIn, "sellIn decreasing test");
-    assertEquals(7, element.quality, "quality decreasing test");
-
-    app.updateQuality();
-    assertEquals(5, element.quality, "negative sellIn / decreasing quality double");
-  
-
   }
 
-  /// 4 Tests
   @Test
-  @DisplayName("Test the quality system of Backstage Passes")
-
-  void testPassesQuality() {
-     Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 17);
-     Item element48 = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 48);
-     Item element49 = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49);
-     Item elementMax = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50);
-
-     for(int expDate = 0; expDate <= 11; expDate++){
-      element.quality = 17;
-      element.sellIn = expDate;
-      element48.quality = 48;
-      element48.sellIn = expDate;
-      element49.quality = 49;
-      element49.sellIn = expDate;
- 
-      elementMax.quality = 50;
-      elementMax.sellIn = expDate;
-
-      GildedRose app = new GildedRose(new Item[] {element});
-      GildedRose app48 = new GildedRose(new Item[] {element48});
-      GildedRose app49 = new GildedRose(new Item[] {element49});
-      GildedRose appMax = new GildedRose(new Item[] {elementMax});
-
-        if(expDate == 0){
-          app.updateQuality();
-          assertEquals(0, element.quality, "The concert is over");
-
-        }
-        else if(expDate <= 5){
-
-          app48.updateQuality();
-          assertEquals(50, element48.quality, "The concert is over");
-          
-
-        }
-        else if(expDate <=10){
-
-          app49.updateQuality();
-          assertEquals(50, element49.quality, "The concert is over");
-
-        }
-        else{
-
-          app49.updateQuality();
-          assertEquals(50, element49.quality, "The concert is over");
-
-        }
-      }
-
-    }
-
-  @Test
-
-  //3 Tests qu'on peut réduire à 2
-
-  @DisplayName("Test the property of Sulfuras")
-  void testSulfurasProperty() {
+  @DisplayName("Test decreasing quality")
+  void diminutionQual(){
     
+    Item element = new Item("foo", 1, 8);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(0, element.sellIn, "sellIn decreasing test");
+  }
+
+  @Test
+  @DisplayName("Test decreasing quality neg")
+  void diminutionsNegQual(){
+    
+    Item element = new Item("foo", 0, 7);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(5, element.quality, "sellIn decreasing test");
+  }
+
+  /* Test functions to test properties of the special item:
+   * "backstage passes to a TAFKAL80ETC concert"
+   * 4 tests. 
+   */
+
+  @Test
+  @DisplayName("Test passes when they expiring")
+  void expiringPasses(){
+
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 17);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(0, element.quality, "0 value backstages test");
+  }
+
+  @Test
+  @DisplayName("Test passes when quality is 48 and sellIn between 1 and 5")
+  void VeryNearPassesWithHighQuality(){
+
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 3, 48);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(50, element.quality, "50 quality test (backstage)");
+  }
+
+  @Test
+  @DisplayName("Test passes when quality is near 49 and sellIn between 6 and 10")
+  void NearPassesWithHighQuality(){
+
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 8, 49);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(50, element.quality, "50 quality test (backstage)");
+  }
+
+  @Test
+  @DisplayName("Test passes when quality is near 49 and sellIn and superior to 10")
+  void PassesWithHighQuality(){
+
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 49);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(50, element.quality, "50 quality test (backstage)");
+  }
+
+
+  /* Test functions to test properties of the special item:
+   * "Sulfuras, Hand of Ragnaros"
+   * 3 tests. 2 optional test 
+   */
+  //Optional test
+  @Test
+  @DisplayName("constant value sulfuras test")
+  void constantValueSulf(){
+
     Item element = new Item("Sulfuras, Hand of Ragnaros", 10, 80);
     GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
     assertEquals(80, element.quality, "constant value sulfuras test");
-    assertEquals(10, element.sellIn, "fixed sellIn test");
+  }
 
-    element.sellIn = -10;
+  @Test
+  //Optional Test
+  @DisplayName("fixed sellIn test")
+  void constantSellInSulf(){
+
+    Item element = new Item("Sulfuras, Hand of Ragnaros", 10, 80);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertEquals(10, element.sellIn, "fixed sellIn test");
+  }
+
+  @Test
+  @DisplayName("contraint value sulfuras (neg) test")
+  void ConstantQualNegSulf() {
+    
+    Item element = new Item("Sulfuras, Hand of Ragnaros", -5, 80);
+    GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
     assertEquals(80, element.quality, "contraint value sulfuras (neg) test");
   }
 
 
-  //4 Test
+  /* Test functions to test properties of the special item:
+   * "Aged Brie"
+   * 4 tests. 2 optional tests (by Jacoco)
+   */
+
+  //Optional Test
   @Test
-  @DisplayName("Test the property of Aged Brie")
-  void testAgedBrie(){
+  @DisplayName("quality increase Brie test")
+  void testqualityIncrBrie(){
+
     Item element = new Item("Aged Brie", 10, 40);
     GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
     assertEquals(41, element.quality, "quality increasing brie test");
+  }
 
-    element.quality = 49;
+  //Optional Test
+  @Test
+  @DisplayName("quality increase Brie test (50)")
+  void testMaxQualityBrie(){
+
+    Item element = new Item("Aged Brie", 10, 49);
+    GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
     assertEquals(50, element.quality, "max quality brie test");
+  }
 
-    element.sellIn = -5;
-    element.quality = 8;
+  @Test
+  @DisplayName("quality neg increase Brie test")
+  void testNegQualityIncrBrie(){
+
+    Item element = new Item("Aged Brie", -5, 8);
+    GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
     assertEquals(10, element.quality, "negative increasing brie Test");
 
-    element.quality = 50;
+  }
+
+  @Test
+  @DisplayName("quality increase Brie test")
+  void testNegMaxQualityBrie(){
+    
+    Item element = new Item("Aged Brie", -5, 50);
+    GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
     assertEquals(50, element.quality, "negative increasing brie Test");
-
   }
 
 }
