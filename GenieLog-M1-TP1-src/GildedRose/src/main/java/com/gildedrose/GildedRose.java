85fd5c7ad81647bde5136a73f07880e025efa55f
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-10-02 09:05:07 +0200

    Remplacement de if imbriqués par switch/case + fonction ensureQualityBounds

--- GenieLog-M1-TP1-src/GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GenieLog-M1-TP1-src/GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,80 +1,80 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
 
-    static public String sulfuras = "Sulfuras, Hand of Ragnaros";
-    static public String backstage = "Backstage passes to a TAFKAL80ETC concert";
-    static public String brie = "Aged Brie";
+  Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
+  static final String sulfuras = "Sulfuras, Hand of Ragnaros";
+  static final String backstage = "Backstage passes to a TAFKAL80ETC concert";
+  static final String brie = "Aged Brie";
+
+  public GildedRose(Item[] items) {
+    this.items = items;
+  }
+
+  public void updateQuality() {
+    for (int i = 0; i < items.length; i++) {
+      Item item = items[i];
+      beforeSellInUpdate(item);
+
+      if (!item.name.equals(sulfuras)) {
+        item.sellIn -= 1;
+      }
+
+      afterSellInUpdate(item);
+      ensureQualityBounds(item);
     }
+  }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            
-            beforeSellInUpdate(i);
-            
-            if (!items[i].name.equals(sulfuras)) {
-                items[i].sellIn -= 1;
-            }
-            
-            afterSellInUpdate(i);
-
+  //Traitement avant mis-à-jour de la date d'expiration
+  private void beforeSellInUpdate(Item item) {
+    switch (item.name) {
+      case brie:
+        item.quality += 1;
+        break;
+      case backstage:
+        if (item.sellIn < 6) {
+          item.quality += 3;
+        } else if (item.sellIn < 11) {
+          item.quality += 2;
+        } else {
+          item.quality += 1;
         }
+        break;
+      case sulfuras:
+        break;
+      default:
+        item.quality -= 1;
+        break;
     }
+  }
 
-    //Traitement avant mis-à-jour de la date d'expiration
-    private void beforeSellInUpdate(int i){
-        
-
-        if (items[i].name.equals(brie) || items[i].name.equals(backstage)) {
-
-            if (items[i].quality < 50) {
-                
-                items[i].quality += 1;
-
-            } if (items[i].name.equals(backstage)) {
-                
-                if (items[i].sellIn < 11 && items[i].quality < 50) {
-                            
-                    items[i].quality += 1;
-                }
-
-                if (items[i].sellIn < 6 && items[i].quality < 50) {
-                            
-                    items[i].quality += 1;
-                }
-            }
-
-        } else if (!items[i].name.equals(sulfuras) && items[i].quality > 0) {
-                
-            items[i].quality -= 1;
-
-        } 
-    } 
-
-    //Traitement après mis-à-jour de la date d'expiration
-    private void afterSellInUpdate(int i){
-
-        if (items[i].sellIn < 0) {
-         
-            if (items[i].name.equals(brie)) {
-                if(items[i].quality < 50){
-                    
-                    items[i].quality += 1;
-                
-                }
-            } else if (items[i].name.equals(backstage)) {
-                  
-                items[i].quality = 0;
-
-            } else if (items[i].quality > 0 && !items[i].name.equals(sulfuras)) {
-                            
-                items[i].quality -= 1;
-                
-            } 
-        }
+  //Traitement après mis-à-jour de la date d'expiration
+  private void afterSellInUpdate(Item item) {
+    if (item.sellIn < 0) {
+      switch (item.name) {
+        case brie:
+          item.quality += 1;
+          break;
+        case backstage:
+          item.quality = 0;
+          break;
+        case sulfuras:
+          break;
+        default:
+          item.quality -= 1;
+          break;
+      }
     }
+  }
+
+  private void ensureQualityBounds(Item item) {
+    if (!item.name.equals(sulfuras)) {
+      if (item.quality < 0) {
+        item.quality = 0;
+      } else if (item.quality > 50) {
+        item.quality = 50;
+      }
+    }
+  }
 }
