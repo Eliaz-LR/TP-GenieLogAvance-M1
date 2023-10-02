8c69a0493b1ab9f4a06ed1dc5b5f332cca4abe28
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-10-02 09:47:58 +0200

    Moved updateQuality to item

--- GenieLog-M1-TP1-src/GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GenieLog-M1-TP1-src/GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -4,62 +4,13 @@ class GildedRose {
 
   Item[] items;
 
-  static final String sulfuras = "Sulfuras, Hand of Ragnaros";
-  static final String backstage = "Backstage passes to a TAFKAL80ETC concert";
-  static final String brie = "Aged Brie";
-
   public GildedRose(Item[] items) {
     this.items = items;
   }
 
   public void updateQuality() {
-    for (int i = 0; i < items.length; i++) {
-      Item item = items[i];
-
-      if (!item.name.equals(sulfuras)) {
-        item.sellIn -= 1;
-        update(item);
-        ensureQualityBounds(item);
-      }
-    }
-  }
-
-  //Traitement avant mis-à-jour de la date d'expiration
-  private void update(Item item) {
-    boolean outOfDate = item.sellIn < 0;
-    switch (item.name) {
-      case brie:
-        item.quality += 1;
-        if (outOfDate) {
-          item.quality += 1;
-        }
-        break;
-      case backstage:
-        if (item.sellIn < 5) {
-          item.quality += 3;
-        } else if (item.sellIn < 10) {
-          item.quality += 2;
-        } else {
-          item.quality += 1;
-        }
-        if (outOfDate) {
-          item.quality = 0;
-        }
-        break;
-      default:
-        item.quality -= 1;
-        if (outOfDate) {
-          item.quality -= 1;
-        }
-        break;
-    }
-  }
-
-  private void ensureQualityBounds(Item item) {
-    if (item.quality < 0) {
-      item.quality = 0;
-    } else if (item.quality > 50) {
-      item.quality = 50;
+    for (Item item : items) {
+      item.updateQuality();
     }
   }
 }
