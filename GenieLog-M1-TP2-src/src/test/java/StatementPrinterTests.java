dc60378e4cd2edf61ea9f8b84d44d03075428c3e
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-10-20 14:20:44 +0200

    Test Fidelity Reduction

--- GenieLog-M1-TP2-src/src/test/java/StatementPrinterTests.java
+++ GenieLog-M1-TP2-src/src/test/java/StatementPrinterTests.java
@@ -39,4 +39,34 @@ public class StatementPrinterTests {
     var result = statementPrinter.toHtml(invoice);
     verify(result);
   }
+
+  @Test
+  void testCustomerFidelityreduction() {
+    Customer customer = new Customer("Good Customer");
+    new Invoice(
+      customer,
+      List.of(
+        new Performance(new Play("Hamlet", Play.PlayType.TRAGEDY), 155),
+        new Performance(new Play("As You Like It", Play.PlayType.COMEDY), 35),
+        new Performance(new Play("Othello", Play.PlayType.TRAGEDY), 60)
+      )
+    );
+
+    // Good customer should now have +167 credits resulting in 15€ reduction for the next invoice
+    System.out.println(
+      "Number of credits after first order : " + customer.credit
+    );
+
+    Invoice invoice2 = new Invoice(
+      customer,
+      List.of(
+        new Performance(new Play("Hamlet", Play.PlayType.TRAGEDY), 55),
+        new Performance(new Play("As You Like It", Play.PlayType.COMEDY), 35),
+        new Performance(new Play("Othello", Play.PlayType.TRAGEDY), 40)
+      )
+    );
+    StatementPrinter statementPrinter = new StatementPrinter();
+    var result2 = statementPrinter.toText(invoice2);
+    verify(result2);
+  }
 }
